de9830341987d617f023be0fd8e73e0cdd104be3
yaninaslab <yana.belash@gmail.com>
2022-05-17 18:02:35 -0400

    Working with localStorage

--- app.js
+++ app.js
@@ -133,3 +133,59 @@ function validate() {
   });
   return isValid;
 }
+
+const list = document.getElementById("todos");
+document.querySelector("button").addEventListener("click", handleClick);
+document.addEventListener("DOMContentLoaded", loadTodos);
+
+function handleClick() {
+  const newTodo = this.previousElementSibling.value.trim();
+
+  if (newTodo) {
+    // add todo
+    createTodo(newTodo);
+    saveToStorage(newTodo);
+    removeFromStorage(newTodo);
+    this.previousElementSibling.value = "";
+  } else {
+    alert("input field is empty ");
+  }
+}
+
+function saveToStorage(todo) {
+  const todos = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  localStorage.setItem("tasks", JSON.stringify([...todos, todo]));
+}
+
+function loadTodos() {
+  const todos = JSON.parse(localStorage.getItem("tasks"));
+
+  if (todos) {
+    todos.forEach((todo) => createTodo(todo));
+  }
+}
+
+function createTodo(text) {
+  const li = document.createElement("li");
+  li.innerText = text;
+  li.className = "todo-item";
+  li.addEventListener("click", removeTodo);
+
+  list.appendChild(li);
+}
+
+function removeFromStorage(removedTodo) {
+  const todos = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify(todos.filter((todo) => todo !== removedTodo))
+  );
+}
+
+function removeTodo() {
+  this.removeEventListener("click", removeTodo);
+  removeFromStorage(this.innerText);
+  this.remove();
+}
