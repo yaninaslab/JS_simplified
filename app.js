2a7c98eb1ba6c7e28fb7e82f5ba5c6eca3361e68
yaninaslab <yana.belash@gmail.com>
2022-05-17 18:28:19 -0400

    Handling events

--- app.js
+++ app.js
@@ -189,3 +189,27 @@ function removeTodo() {
   removeFromStorage(this.innerText);
   this.remove();
 }
+
+document.getElementById("myBtn").addEventListener("click", toggleSpoiler);
+
+function toggleSpoiler() {
+  const spoiler = document.getElementById("spoiler");
+  spoiler.classList.toggle("closed");
+
+  if (spoiler.classList.contains("closed")) {
+    dettachSpoilerEvents();
+  } else {
+    attachSpoilerEvents();
+  }
+}
+function attachSpoilerEvents() {
+  document.addEventListener("keydown", handleEscape);
+}
+
+function dettachSpoilerEvents() {
+  document.removeEventListener("keydown", handleEscape);
+}
+
+function handleEscape(event) {
+  if (event.key === "Escape") toggleSpoiler();
+}
